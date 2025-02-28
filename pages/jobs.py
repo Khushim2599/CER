170f01f37ebdc73f3b7fb9b6c4e043b254ac29ee
Khushim2599 <102567984+Khushim2599@users.noreply.github.com>
2025-02-28 14:43:17 -0500

    Update jobs.py

--- pages/jobs.py
+++ pages/jobs.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Job & Career Help", page_icon="💼")
+
+st.title("🔹 Job & Career Help")
+st.markdown("""
+- [Goodwill Job Training](https://www.goodwillng.org/job-training)
+- [Georgia WorkSource](https://www.worksourceatlanta.org/)
+- [Resume Templates (Canva)](https://www.canva.com/resumes/templates/)
+- [Latino Jobs Network](https://latinojobsnetwork.com/)
+""")
 
