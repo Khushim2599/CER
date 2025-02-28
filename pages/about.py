2cae131f9e4a5f2d7e76af05492ae68ad5486d3c
Khushim2599 <102567984+Khushim2599@users.noreply.github.com>
2025-02-28 15:06:10 -0500

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+# Add a button at the top to go back to Home Page
+if st.button("🏠 Go to Home Page"):
+    st.switch_page("app.py")
+
 st.set_page_config(page_title="About Us", page_icon="📢")
 
 st.title("📢 About Us")
