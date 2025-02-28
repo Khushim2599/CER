226c9521e5702c370a4345004dc17bcf56ac4986
Khushim2599 <102567984+Khushim2599@users.noreply.github.com>
2025-02-28 14:43:31 -0500

    Update education.py

--- pages/education.py
+++ pages/education.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Education Resources", page_icon="📚")
+
+st.title("📚 Education Resources")
+st.markdown("""
+- [Atlanta Technical College ESL](https://www.atlantatech.edu/)
+- [GED Classes in Atlanta](https://www.ged.com/)
+- [Free English Classes in GA](https://www.esl.com/resources/georgia/)
+""")
 
