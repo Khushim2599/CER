5f56f43443066c29a1a6f34783a0ef25b9a9ec4f
Khushim2599 <102567984+Khushim2599@users.noreply.github.com>
2025-02-28 15:15:23 -0500

    Update jobs.py

--- pages/jobs.py
+++ pages/jobs.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+# Go to Home Page button
+if st.button("🏠 Go to Home Page"):
+    st.switch_page("app.py")
+
 st.set_page_config(page_title="Job & Career Help", page_icon="💼")
 
 st.title("🔹 Job & Career Help")
