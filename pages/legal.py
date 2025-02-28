726f1669e2a05904db1979a90885842baac6bd39
Khushim2599 <102567984+Khushim2599@users.noreply.github.com>
2025-02-28 14:43:43 -0500

    Update legal.py

--- pages/legal.py
+++ pages/legal.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Legal & Immigration Help", page_icon="⚖️")
+
+st.title("⚖️ Legal & Immigration Help")
+st.markdown("""
+- [Georgia Latino Alliance for Human Rights](https://glahr.org/)
+- [Atlanta Legal Aid](https://atlantalegalaid.org/)
+- [USCIS Work Authorization](https://www.uscis.gov/working-in-the-united-states)
+""")
 
